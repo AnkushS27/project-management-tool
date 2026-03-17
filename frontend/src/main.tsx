7d7ad4fb93678e4e771b36a024790985d3df65ab
Ankush Singh <ankush27singh11@gmail.com>
2026-03-17 17:54:41 +0530

    fix: lib fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux'
 import './index.css'
 import App from './App.tsx'
 import { store } from './app/store'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
